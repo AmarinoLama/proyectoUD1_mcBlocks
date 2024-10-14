f4adedb18c3cf044e620f0fdf9a68e8651e0cffb
AmarinoLama <amarinolama@gmail.com>
2024-10-14 23:04:24 +0200

    feat() : crear la ventana emergente con una label

--- src/main/java/edu/badpals/proyectoud1_mcrecipes/Main.java
+++ src/main/java/edu/badpals/proyectoud1_mcrecipes/Main.java
@@ -1,25 +1,27 @@
 package edu.badpals.proyectoud1_mcrecipes;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        String output = "salida en pantalla";
+        stage.setTitle("Minecraft Recipes");
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle(output);
+        // mostrar una laberl con texto
+        Label label = new Label("Mostrar recipes");
+        Scene scene = new Scene(label, 400, 200);
         stage.setScene(scene);
+
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        Application.launch(args);
     }
 }
