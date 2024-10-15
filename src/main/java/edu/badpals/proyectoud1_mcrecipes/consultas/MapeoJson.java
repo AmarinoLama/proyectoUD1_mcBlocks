e110a1f78615b927e472742ad6f69a38336706c3
AmarinoLama <amarinolama@gmail.com>
2024-10-15 19:55:25 +0200

    refactor(): crear métodos para las recetas y los items

--- src/main/java/edu/badpals/proyectoud1_mcrecipes/consultas/MapeoJson.java
+++ src/main/java/edu/badpals/proyectoud1_mcrecipes/consultas/MapeoJson.java
@@ -1,10 +1,24 @@
 package edu.badpals.proyectoud1_mcrecipes.consultas;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.badpals.proyectoud1_mcrecipes.objetos.Recipes;
+import edu.badpals.proyectoud1_mcrecipes.objetos.Item;
+import edu.badpals.proyectoud1_mcrecipes.objetos.Recipe;
 
 public class MapeoJson {
+
     public static void main(String[] args) throws Exception {
+        Recipe[] recipes = mapingRecipes();
+        Item[] items = mapingItems();
+        for (Recipe recipe : recipes) {
+            System.out.println(recipe.getRecipe());
+        }
+        for (Item item : items) {
+            System.out.println(item.getImage());
+        }
+    }
+
+    public static Recipe[] mapingRecipes() throws Exception {
         // Crear un ObjectMapper
         ObjectMapper mapper = new ObjectMapper();
 
@@ -12,11 +26,24 @@ public class MapeoJson {
         String json = ApiRequest.recipeRequest();
 
         // Mapear el JSON a una instancia de Recipes
-        Recipes[] recipes = mapper.readValue(json, Recipes[].class);
+        Recipe[] recipes = mapper.readValue(json, Recipe[].class);
 
-        // Ahora puedes usar el objeto recipes como desees
-        for (Recipes recipe : recipes) {
-            System.out.println(recipe.getQuantity());
-        }
+        return recipes;
+    }
+
+    public static Item[] mapingItems() throws Exception {
+        // Crear un ObjectMapper
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Ignorar propiedades excesivas
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        // Obtener el JSON de la respuesta
+        String json = ApiRequest.itemRequest();
+
+        // Mapear el JSON a una instancia de Items
+        Item[] items = mapper.readValue(json, Item[].class);
+
+        return items;
     }
 }
