268a5f0eb1586d3f7b461d69d709da223960c2d3
AmarinoLama <amarinolama@gmail.com>
2024-10-15 18:40:33 +0200

    config(): añadir dependencias

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module edu.badpals.proyectoud1_mcrecipes {
     requires com.fasterxml.jackson.databind;
     requires java.net.http;
 
-
     opens edu.badpals.proyectoud1_mcrecipes to javafx.fxml;
+    opens edu.badpals.proyectoud1_mcrecipes.objetos to com.fasterxml.jackson.databind;
     exports edu.badpals.proyectoud1_mcrecipes;
 }
