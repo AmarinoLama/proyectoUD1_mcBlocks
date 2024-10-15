41ec25a7d88e57ac86410c6f6d9b1d8f4334bfe3
AmarinoLama <amarinolama@gmail.com>
2024-10-15 18:14:15 +0200

    config() : modificar el pom para poder usar HttpClient

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module edu.badpals.proyectoud1_mcrecipes {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires com.fasterxml.jackson.databind;
+    requires java.net.http;
 
 
     opens edu.badpals.proyectoud1_mcrecipes to javafx.fxml;
