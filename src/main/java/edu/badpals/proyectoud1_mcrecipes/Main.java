e56d92fc24bcecbf99d50a2d742ff8045b1ea761
AmarinoLama <amarinolama@gmail.com>
2024-10-16 20:48:08 +0200

    feat() : hacer que las imagenes se cargen al lanzar el recip

--- src/main/java/edu/badpals/proyectoud1_mcrecipes/Main.java
+++ src/main/java/edu/badpals/proyectoud1_mcrecipes/Main.java
@@ -6,25 +6,40 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
+
+import static edu.badpals.proyectoud1_mcrecipes.login.Login.creadorProperties;
+import static edu.badpals.proyectoud1_mcrecipes.login.Login.validatePass;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
         FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("/mainview.fxml")));
         Parent root = loader.load();
         Scene scene = new Scene(root);
 
         stage.setTitle("Minecraft Recipes");
         stage.setResizable(false);
+
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         Application.launch(args);
+        Minimain();
+    }
+
+    public static void Minimain() throws FileNotFoundException, NoSuchAlgorithmException {
+
+        creadorProperties();
+
+        System.out.println(validatePass("Administrador","suputamadre"));
     }
 
 }
--- src/main/java/edu/badpals/proyectoud1_mcrecipes/controlls/MainController.java
+++ src/main/java/edu/badpals/proyectoud1_mcrecipes/controlls/MainController.java
@@ -1,13 +1,19 @@
 package edu.badpals.proyectoud1_mcrecipes.controlls;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import edu.badpals.proyectoud1_mcrecipes.consultas.ApiRequest;
 import edu.badpals.proyectoud1_mcrecipes.consultas.MapeoJson;
+import edu.badpals.proyectoud1_mcrecipes.objetos.Item;
 import edu.badpals.proyectoud1_mcrecipes.objetos.Recipe;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 import javafx.event.ActionEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.ArrayList;
 
 public class MainController {
 
@@ -18,23 +24,135 @@ public class MainController {
     private TextField txtRecip;
 
     @FXML
-    private TextField txtOutput;
+    private ImageView imgArrow;
+
+    @FXML
+    private ImageView imgRecip;
+
+    @FXML
+    private ImageView imgTopCenter;
+
+    @FXML
+    private ImageView imgTopLeft;
+
+    @FXML
+    private ImageView imgTopRight;
+
+    @FXML
+    private ImageView imgbotton;
+
+    @FXML
+    private ImageView imgbottonleft;
+
+    @FXML
+    private ImageView imgbottonright;
+
+    @FXML
+    private ImageView imgcenter;
+
+    @FXML
+    private ImageView imgmidleft;
+
+    @FXML
+    private ImageView imgmidright;
 
     @FXML
     void btnClicked(ActionEvent event) throws Exception {
+
         ApiRequest.setItem(getTxtRecip());
-        Recipe[] recipes = MapeoJson.mapingRecipes();
-        System.out.printf(recipes[0].getRecipe().toString());
-        setTxtOutput(txtOutput);
-        // devuelve un print de la recip
+        Recipe recipe = MapeoJson.mapingRecipes()[0];
+
+        ArrayList<String> itemsSearch = new ArrayList<>();
+        itemsSearch.add(recipe.getItem());
+
+        for (JsonNode item : recipe.getRecipe()) {
+            itemsSearch.add(item.toString().replace("\"", ""));
+        }
+
+        ArrayList<String> itemsImg = new ArrayList<>();
+        for (String item : itemsSearch) {
+            if (!item.equals("null")) {
+                ApiRequest.setItem(item);
+                Item itemtoSearch = MapeoJson.mapingItems()[0];
+                itemsImg.add(itemtoSearch.getImage());
+            } else {
+                itemsImg.add("https://minecraft-api.vercel.app/images/blocks/air.png");
+            }
+        }
+
+        setImgRecip(itemsImg.get(0));
+        setImgArrow();
+        setImgTopLeft(itemsImg.get(1));
+        setImgTopCenter(itemsImg.get(2));
+        setImgTopRight(itemsImg.get(3));
+        setImgmidleft(itemsImg.get(4));
+        setImgcenter(itemsImg.get(5));
+        setImgmidright(itemsImg.get(6));
+        setImgbottonleft(itemsImg.get(7));
+        setImgbotton(itemsImg.get(8));
+        setImgbottonright(itemsImg.get(9));
     }
 
     public String getTxtRecip() {
         return txtRecip.getText();
     }
 
-    public void setTxtOutput(TextField txtOutput) {
-        this.txtOutput = txtOutput;
-        txtOutput.setText("Hola");
+    public ImageView getImgRecip() {
+        return imgRecip;
+    }
+
+    public void setImgRecip(String url) {
+        this.imgRecip = imgRecip;
+        imgRecip.setImage(new Image(url));
+    }
+
+    public void setImgArrow() {
+        Image image = new Image("/flecha.png");
+        this.imgArrow.setImage(image);
+    }
+
+    public void setImgTopCenter(String url) {
+        Image image = new Image(url);
+        this.imgTopCenter.setImage(image);
+    }
+
+    public void setImgTopLeft(String url) {
+        Image image = new Image(url);
+        this.imgTopLeft.setImage(image);
+    }
+
+    public void setImgTopRight(String url) {
+        Image image = new Image(url);
+        this.imgTopRight.setImage(image);
+    }
+
+    public void setImgbotton(String url) {
+        Image image = new Image(url);
+        this.imgbotton.setImage(image);
+    }
+
+    public void setImgbottonleft(String url) {
+        Image image = new Image(url);
+        this.imgbottonleft.setImage(image);
+    }
+
+    public void setImgbottonright(String url) {
+        Image image = new Image(url);
+        this.imgbottonright.setImage(image);
+    }
+
+    public void setImgmidright(String url) {
+        Image image = new Image(url);
+        this.imgmidright.setImage(image);
+    }
+
+    public void setImgmidleft(String url) {
+        Image image = new Image(url);
+        this.imgmidleft.setImage(image);
+    }
+
+    public void setImgcenter(String url) {
+        Image image = new Image(url);
+        this.imgcenter.setImage(image);
     }
 }
