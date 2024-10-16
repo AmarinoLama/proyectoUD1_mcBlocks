5724acfb3c6e38f79eb01b7edc51f2c4542de3b3
amarinolama <amarinolama@gmail.com>
2024-10-16 12:52:21 +0200

    config() : cambiar el module-info y arreglar el mainview

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,13 @@
 module edu.badpals.proyectoud1_mcrecipes {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
     requires com.fasterxml.jackson.databind;
     requires java.net.http;
 
     opens edu.badpals.proyectoud1_mcrecipes to javafx.fxml;
     opens edu.badpals.proyectoud1_mcrecipes.controlls to javafx.fxml;
+    opens edu.badpals.proyectoud1_mcrecipes.objetos to com.fasterxml.jackson.databind;
 
     exports edu.badpals.proyectoud1_mcrecipes;
+    exports edu.badpals.proyectoud1_mcrecipes.objetos;
 }
